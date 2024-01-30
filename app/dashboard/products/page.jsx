8b0b012e2e3b59f779704beb810b05ea26d5f995
ThomassMathisen <thomasmathisen@hotmail.com>
2024-01-30 17:58:12 +0100

    Creates users and products pages

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import styles from "@/app/ui/dashboard/products/products.module.css";
+import Search from "@/app/ui/dashboard/search/search";
+import Image from "next/image";
+import Link from "next/link";
 
 const ProductsPage = () => {
-  return <div>Products</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search for a product" />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created At</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.productImage}
+                />
+                Playstation 5
+              </div>
+            </td>
+            <td>Newest gaming console</td>
+            <td>$499</td>
+            <td>01.01.2023</td>
+            <td>99+</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href={"/dashboard/products/1"}>
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <form>
+                  <input type="hidden" name="id" />
+                  <button className={`${styles.button} ${styles.delete}`}>
+                    Delete
+                  </button>
+                </form>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 };
 
 export default ProductsPage;
